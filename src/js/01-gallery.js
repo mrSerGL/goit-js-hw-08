efad86e9f147e815d5b2d55243bc2b4a50f5ae54
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-02-28 19:54:31 +0200

    simplelight install

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,79 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+let instanceBasicLightbox;
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGallуryItem = event.target.classList.contains("gallery__image");
+
+  if (!isGallуryItem) {
+    return;
+  }
+
+  // openModal();
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    captionsData: 'alt', captionDelay: 250, 
+
+  });
+}
+
+function openModal() {
+ // let instance = basicLightbox
+    // .create(
+    //   `<img width="1400" height="900" src="${event.target.dataset.source}">`
+    // )
+    // .show();
+
+    
+
+  // instanceBasicLightbox = instance;
+  // window.addEventListener("keydown", onEscKeyPress);
+
+  
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    const divModal = document.querySelector(".basicLightbox");
+
+    divModal.classList.remove("basicLightbox--visible");
+
+    window.removeEventListener("keydown", onEscKeyPress);
+  }
+}
