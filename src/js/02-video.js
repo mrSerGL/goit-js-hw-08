852fee86c6a260b611a336ca10e78be69d8d2634
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-02 11:21:07 +0200

    video complit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-console.log(iframe);
 
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
+
+const pausedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(pausedTime);
