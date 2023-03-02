90cf07f122fb0ee22cb1af58b675c7bd5f34112c
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-02 16:41:27 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const inputEmailRef = document.querySelector('.feedback-form input');
+const inputMessageRef = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+restoreInputsData();
+
+formRef.addEventListener('input', throttle(saveInputsData, 1000));
+formRef.addEventListener('submit', onFormSubmit);
+
+function saveInputsData(event) {
+  const formData = {};
+
+  formData.email = inputEmailRef.value;
+  formData.message = inputMessageRef.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function restoreInputsData() {
+  const restoredFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (restoredFormData) {
+    inputEmailRef.value = restoredFormData.email;
+    inputMessageRef.value = restoredFormData.message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
